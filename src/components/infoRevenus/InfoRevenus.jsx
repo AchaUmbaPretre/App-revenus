fac307a4d1fb2a8229b5fafc8f8cc9e73af2c858
AchaUmbaPretre <achandambi@gmail.com>
2024-07-24 09:44:52 +0100

    commit

--- src/components/infoRevenus/InfoRevenus.jsx
+++ src/components/infoRevenus/InfoRevenus.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { VerticalAlignBottomOutlined, VerticalAlignTopOutlined } from '@ant-design/icons';
 import './infoRevenus.scss'
+import axios from 'axios';
+import config from '../../config';
+import CountUp from 'react-countup';
 
 const InfoRevenus = () => {
+    const [venteTotal, setVenteTotal] = useState([]);
+    const [produitTotalAchats, setProduitTotalAchats] = useState([]);
+    const [depenses, setDepenses] = useState([]);
+    const [depensesFalcon, setDepenseFalcon] = useState([]);
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+    const DOMAINFALCON = config.REACT_APP_SERVER_DOMAIN_FALCON;
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/api/rapport/venteTotal/total`);
+            setVenteTotal(data[0]?.montant_total_vente);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN]);
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAIN}/api/depenses/depenseCount`);
+            setDepenses(data[0]?.total_depense);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAIN]);
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data } = await axios.get(`${DOMAINFALCON}/depense/count1an`);
+            setDepenseFalcon(data[0]?.total_depense);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        fetchData();
+      }, [DOMAINFALCON]);
+
   return (
     <>
         <div className="info-revenus">
@@ -14,7 +60,7 @@ const InfoRevenus = () => {
                             <VerticalAlignBottomOutlined className='info-icon' style={{color: 'rgba(53, 52, 52, 0.719)'}} />
                         </div>
                         <div className="info-row-right">
-                            <h2 className="info-row-h2">1000$</h2>
+                            <h2 className="info-row-h2"><CountUp end={venteTotal}/>$</h2>
                             <span className="info-span">Montant total de la vente</span>
                         </div>
                     </div>
@@ -26,7 +72,7 @@ const InfoRevenus = () => {
                             <VerticalAlignTopOutlined className='info-icon' style={{color: 'red'}}/>
                         </div>
                         <div className="info-row-right">
-                            <h2 className="info-row-h2">1000$</h2>
+                            <h2 className="info-row-h2"><CountUp end={depenses}/>$</h2>
                             <span className="info-span">Montant total des dépenses</span>
                         </div>
                     </div>
@@ -38,7 +84,7 @@ const InfoRevenus = () => {
                             <VerticalAlignBottomOutlined className='info-icon' style={{color: 'rgba(53, 52, 52, 0.719)'}} />
                         </div>
                         <div className="info-row-right">
-                            <h2 className="info-row-h2">1000$</h2>
+                            <h2 className="info-row-h2"><CountUp end={1000}/>$</h2>
                             <span className="info-span">Montant total des paiements</span>
                         </div>
                     </div>
@@ -50,7 +96,7 @@ const InfoRevenus = () => {
                             <VerticalAlignTopOutlined className='info-icon' style={{color: 'red'}}/>
                         </div>
                         <div className="info-row-right">
-                            <h2 className="info-row-h2">1000$</h2>
+                            <h2 className="info-row-h2"><CountUp end={depensesFalcon}/>$</h2>
                             <span className="info-span">Montant total des dépenses</span>
                         </div>
                     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.scss'
 import InfoRevenus from '../../components/infoRevenus/InfoRevenus'
 import ChartFalcon from '../../components/chartFalcon/ChartFalcon'
 import ChartNdoe from '../../components/chartNdoe/ChartNdoe'
+import { HomeOutlined } from '@ant-design/icons';
+import { Select } from 'antd'
+const { Option } = Select;
 
 const Home = () => {
+    const [dateFilter, setDateFilter] = useState('today');
+
+    const handleDateFilterChange = (value) => {
+        setDateFilter(value);
+/*         fetchData(value); */
+      };
+
   return (
     <>
         <div className="home">
+            <div className="home-rapport">
+                <div className="home-left">
+                    <HomeOutlined className='icon-home' />
+                    <span className="home-span">ACCUEIL</span>
+                </div>
+                <div className="home-right">
+                    <Select value={dateFilter} onChange={handleDateFilterChange} style={{ width: 200 }}>
+                        <Option value="today">Aujourd'hui</Option>
+                        <Option value="yesterday">Hier</Option>
+                        <Option value="last7days">7 derniers jours</Option>
+                        <Option value="last30days">30 derniers jours</Option>
+                        <Option value="last1year">1 an</Option>
+                    </Select>
+                </div>
+            </div>
             <div className="home-wrapper">
                 <InfoRevenus/>
             </div>
